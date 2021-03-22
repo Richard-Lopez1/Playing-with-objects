2b601e0948be455ac198fc062807ad31d0c5fbf7
Richard Lopez <78048220+Richard-Lopez1@users.noreply.github.com>
2021-03-22 10:25:54 -0700

    answers for JS assignment playing with objects

---
+++ playing_with_objects.js
@@ -0,0 +1,35 @@
+var users = [
+    {
+        name: "Michael",
+        age: 37
+    },
+    {
+        name: "John",
+        age: 30
+    },
+    {
+        name: "David",
+        age: 27
+    }
+];
+
+// How would you print/log John's age?
+console.log(users[1].age);
+
+
+// How would you print/log the name of the first object?
+console.log(users[0].name);
+
+// How would you print/log the name and age of each user using a for loop?  
+// Your output should look something like this
+// Michael - 37
+// John - 30
+// David - 27
+
+var rick = 0;
+	for (var num in users){
+ 	 console.log(users[num].name, '-', users[num].age);
+	    num++;
+    
+ 	 }
+   
